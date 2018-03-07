2576a6fa5784341de66a59a78ea3e3ecf6953f50
Jacob R. Snover <snoverjacob@yahoo.com>
2018-03-06 21:08:37 -0500

    code update

---
+++ MVCBreakout/Controllers/HomeController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.Entity;
+using MVCBreakout.Models;
+
+
+namespace MVCBreakout.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            LearnerEntities ORM = new LearnerEntities();
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult AddEmp(Employee data)
+        {
+            LearnerEntities ORM = new LearnerEntities();
+            ORM.Employees.Add(data);
+            ORM.SaveChanges();
+            ViewBag.VerifyAdd = data.FirstName;
+            
+            return View("About");
+        }
+
+        public ActionResult AddPos(Position data)
+        {
+            LearnerEntities ORM = new LearnerEntities();
+            ORM.Positions.Add(data);
+            ORM.SaveChanges();
+            ViewBag.VerifyAdd = data.Name;
+
+            return View("About");
+        }
+
+        public ActionResult AddCour(Course data)
+        {
+            LearnerEntities ORM = new LearnerEntities();
+            ORM.Courses.Add(data);
+            ORM.SaveChanges();
+            ViewBag.VerifyAdd = data.Name;
+
+            return View("About");
+        }
+
+        public ActionResult AddStudent(Student data)
+        {
+            LearnerEntities ORM = new LearnerEntities();
+            ORM.Students.Add(data);
+            ORM.SaveChanges();
+            ViewBag.VerifyAdd = data.FirstName;
+
+            return View("About");
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ MVCBreakout/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCBreakout.MvcApplication" Language="C#" %>
